2e00a951c6a5b1969a4cb9199a76a3546e28ec2c
Anyrii <167205612+Anyrii@users.noreply.github.com>
2024-08-20 11:44:17 +0800

    Update config.js
    changed sheetID

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "123scaAx8MtPwVEzi-Dxb6u2Fr-2FDBp1R7Y8KjWXPPw";
 
 
 /* ------------------------------------------------------------------- */
